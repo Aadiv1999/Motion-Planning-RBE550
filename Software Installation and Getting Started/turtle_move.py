004ccc06bad20accf665171ccd662a67082ece90
Aadiv <aadivshah99@gmail.com>
2022-01-23 14:33:37 -0500

    turtle star

--- Software Installation and Getting Started/turtle_move.py
+++ Software Installation and Getting Started/turtle_move.py
@@ -2,10 +2,26 @@ from turtle import *
 
 color('red', 'yellow')
 begin_fill()
+
+# stop drawing
+penup()
+
+# move to initial position to center of the screen
+left(180)
+forward(250)
+right(180-36)
+
+# get the start position
+start = pos()
+
+# start drawing
+pendown()
 while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
+    forward(500)
+    right(180-36)
+
+    if abs(pos()-start) < 1:
         break
+
 end_fill()
 done()
