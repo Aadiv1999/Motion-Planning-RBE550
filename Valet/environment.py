a854d7eaeeef27bc5e42e6fc43c608870c318dfa
Aadiv <aadivshah99@gmail.com>
2022-03-10 18:01:18 -0500

    collision checking

--- Valet/environment.py
+++ Valet/environment.py
@@ -1,4 +1,6 @@
+from cmath import isclose
 from mouse import mouse_trajectory
+from collision import intersects
 import time
 from typing import List, Tuple, Union
 from copy import copy
@@ -13,6 +15,13 @@ def max_speed(speed):
         return 1
     else:
         return speed
+    
+def max_turn(speed):
+    thresh = 0.02
+    if abs(speed) > thresh:
+        return thresh
+    else:
+        return abs(speed)
 
 
 def rot_points(mat, degrees: float):
@@ -31,8 +40,8 @@ class Robot:
     y_coord: int
     vel_r: int
     vel_l: int
-    width: int = 20
-    height: int = 30
+    width: int = 20*2
+    height: int = 30*2
     angle = 0
     points = []
     dt = 0 
@@ -72,6 +81,18 @@ class Robot:
         self.vel_r = vel_r
         self.vel_l = vel_l
     
+    def turn(self, diff_heading, turn_speed) -> None:
+        if diff_heading >= 0:
+            if diff_heading <=180:
+                self.move(turn_speed, -turn_speed)
+            else:
+                self.move(-turn_speed, turn_speed)
+        else:
+            if diff_heading <= -180:
+                self.move(turn_speed, -turn_speed)
+            else:
+                self.move(-turn_speed, turn_speed)
+    
     def get_position(self) -> Tuple[float, float]:
         return self.x_coord, self.y_coord
     
@@ -103,25 +124,33 @@ class Controller:
     
 
 class World:    
-    obstacle = []
     obs = np.array([[200,100],
                     [-200,100],
                     [-200,-100],
                     [200,-100],
                     [200,100]])
-    vehicle1 = []
-    vehicle2 = []
+    vehicle1 = np.array([[100,50],
+                    [-100,50],
+                    [-100,-50],
+                    [100,-50],
+                    [100,50]])
+    vehicle2 = np.array([[100,50],
+                    [-100,50],
+                    [-100,-50],
+                    [100,-50],
+                    [100,50]])
 
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.obs = self.obs + np.array([600,400])
+        self.obs += convert_to_display(np.array([[600,700]]))
+        self.vehicle1 += convert_to_display(np.array([[600,100]]))
+        self.vehicle2 += convert_to_display(np.array([[100,100]]))
+        self.obstacles = [self.obs, self.vehicle1, self.vehicle2]
         
         
-class Planner:
-
-    
-    def __init__(self) -> None:
+class Planner:    
+    def __init__(self, robot: Robot, world: World) -> None:
         self.trajectory = [[500, 800],
                             [900,800],
                             [800, 500],
@@ -129,6 +158,8 @@ class Planner:
                             [900, 900],
                             [500,500],
                             [100, 100]]
+        self.robot = robot
+        self.world = world
 
     def get_trajectory(self):
         return self.trajectory
@@ -141,14 +172,36 @@ class Planner:
         else:
             return heading
 
+    # if robot waypoint is in collision skip that waypoint
+    def is_collision(self) -> bool:
+        # get robot segments
+        points = self.robot.get_robot_points()
+
+        seg1 = (points[0], points[1])
+        seg2 = (points[1], points[2])
+        seg3 = (points[2], points[3])
+        seg4 = (points[3], points[4])
+        seg5 = (points[4], points[0])
+        
+        # check if any collide
+        for obs in self.world.obstacles:
+            for i in range(len(obs)):
+                if i==len(obs)-1:
+                    o = (obs[i], obs[0])
+                else:
+                    o = (obs[i], obs[i+1])
+                if intersects(o, seg1) or intersects(o, seg2) or intersects(o, seg3) or \
+                        intersects(o, seg4) or intersects(o, seg5):
+                    return True
+
+
+
+
 class Visualizer:
     BLACK: Tuple[int, int, int] = (0, 0, 0)
     RED: Tuple[int, int, int] = (255, 0, 0)
     WHITE: Tuple[int, int, int] = (255, 255, 255)
     BLUE: Tuple[int, int, int] = (0, 0, 255)
-    TREE_GREEN: Tuple[int, int, int] = (34, 139, 34)
-    CROP_GREEN: Tuple[int, int, int] = (167, 199, 155)
-    WEED_GREEN: Tuple[int, int, int] = (20, 66, 6)
 
     def __init__(self, robot: Robot, world: World, planner: Planner) -> None:
         pygame.init()
@@ -187,12 +240,18 @@ class Visualizer:
         for i in range(len(self.world.obs)-1):
             pygame.draw.line(self.screen, self.RED, self.world.obs[i], self.world.obs[i+1], 8)
         
+        for i in range(len(self.world.vehicle1)-1):
+            pygame.draw.line(self.screen, self.RED, self.world.vehicle1[i], self.world.vehicle1[i+1], 8)
+        
+        for i in range(len(self.world.vehicle2)-1):
+            pygame.draw.line(self.screen, self.RED, self.world.vehicle2[i], self.world.vehicle2[i+1], 8)
+        
         traj = convert_to_display(np.array(self.planner.get_trajectory()))
 
         for i in range(len(traj)-1):
             pygame.draw.line(self.screen, self.BLUE, traj[i], traj[i+1])
 
-    def update_display(self) -> bool:
+    def update_display(self, is_colliding) -> bool:
 
         self.screen.fill(self.WHITE)
 
@@ -205,7 +264,12 @@ class Visualizer:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE: # if escape is pressed, quit the program
                     return False
-
+        
+        if is_colliding:
+            collide = self.font.render("Collision", True, self.BLACK)
+        else:
+            collide = self.font.render("No Collision", True, self.BLACK)
+        self.screen.blit(collide, (500, 5))
         pygame.display.flip()
 
         return True
@@ -225,17 +289,16 @@ class Runner:
 
     def run(self, trajectory):
         self.planner.trajectory = convert_to_display( np.array(trajectory))
-        x = self.robot.x_coord
-        y = self.robot.y_coord
         running = True
         counter = 0
         last_counter = -1
         lasttime = pygame.time.get_ticks() 
-        success = False     
+        success = False    
+        is_colliding = False 
         goal = self.planner.get_trajectory()
 
         while running:
-            
+
             self.controller.step()
 
             if counter < len(goal):
@@ -243,62 +306,52 @@ class Runner:
             else:
                 success = True
                 print("Final goal reached")
+                time.sleep(5)
+                break
+
             
             if success:
                 # do stuff for new goal
                 # stop robot
                 self.robot.move(0,0)
-                # if counter < len(goal):
-                #     self.robot.set_position(goal[counter])
-                counter += 1
-                
-                print("Waypoint reached")
-                print(self.robot.get_position())
+                counter += 1                
+                # print("Waypoint reached")
+
+                # check collision here
+                # if collision -> counter++
+                # till new waypoint is w/o collision
+                # while self.planner.is_collision(goal, counter):
+                #     counter += 1
             else:
-                # turn towards goal
-                if counter == 0:
-                    dy = goal[counter][1] - y
-                    dx = goal[counter][0] - x
-                    last_counter = counter
+                if self.planner.is_collision():
+                    is_colliding = True
                 else:
-                    # dy = goal[counter][1] - goal[counter-1][1]
-                    # dx = goal[counter][0] - goal[counter-1][0]
-                    if counter != last_counter:
-                        dy = goal[counter][1] - self.robot.y_coord
-                        dx = goal[counter][0] - self.robot.x_coord
-                        last_counter = counter
+                    is_colliding = False
+                
+                if counter != last_counter:
+                    dy = goal[counter][1] - self.robot.y_coord
+                    dx = goal[counter][0] - self.robot.x_coord
+                    last_counter = counter
                 
                 heading = np.round(self.planner.get_heading(dx,dy),0)
                 diff_heading = self.robot.angle - heading
                 
                 if abs(diff_heading) > 0.05:
-                    turn_speed = 0.01
-                    if diff_heading >= 0:
-                        if diff_heading <=180:
-                            self.robot.move(turn_speed, -turn_speed)
-                        else:
-                            self.robot.move(-turn_speed, turn_speed)
-                    else:
-                        if diff_heading <= -180:
-                            self.robot.move(turn_speed, -turn_speed)
-                        else:
-                            self.robot.move(-turn_speed, turn_speed)
+                    gain = 0.01
+                    turn_speed = max_turn(gain * diff_heading)
+                    self.robot.turn(diff_heading, turn_speed)
                 else:
                     # self.robot.angle = heading
                     gain = 0.005
                     speed = max_speed(gain*((self.robot.x_coord-goal[counter][0])**2 + (self.robot.y_coord-goal[counter][1])**2)**0.5)
                     self.robot.move(speed, speed)
-                
-
-
-
-            
+                    
 
             # dt            
             self.robot.dt = (pygame.time.get_ticks() - lasttime)
             lasttime = pygame.time.get_ticks()
 
-            running = self.vis.update_display()
+            running = self.vis.update_display(is_colliding)
             
             time.sleep(0.001)
         
@@ -311,16 +364,14 @@ def main():
     robot = Robot(0,900,0)
     controller = Controller(robot)
     world = World(width, height)
-    planner = Planner()
+    planner = Planner(robot, world)
     vis = Visualizer(robot, world, planner)
 
     runner = Runner(robot, controller, world, planner, vis)
 
     try:
         trajectory = mouse_trajectory(world.obs, width, height)
-        print(len(trajectory))
         traj = trajectory[1::3]
-        print(len(traj))
         runner.run(traj)
     except AssertionError as e:
         print(f'ERROR: {e}, Aborting.')
